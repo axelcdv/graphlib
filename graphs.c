9ef64da77afb37cc4195a8b8be58b5ddc90a9f4f
Axel Colin de Verdiere <axel@axelcdv.com>
2013-03-10 18:20:10 +0100

    Changes:  - DFS done  - BFS template Warning:  - not all edges are shown by create_random_graph, example if 3->1 is added while processing 3, 1->3 will not be shown

--- graphs.c
+++ graphs.c
@@ -4,48 +4,97 @@
 #include <time.h>
 #include <stdbool.h>
 
-void process_vertex_early (Graph g, int v)
+/**
+ * Template function to process a vertex upon first entry
+ * @param g graph representation
+ * @param v vertex to process
+ */
+void process_vertex_early (Graph *g, int v)
 {
 	printf("Processing vertex %d early\n", v);
 	return;
 }
 
-void process_vertex_late (Graph g, int v)
+/**
+ * Template function to process a vertex upon leaving
+ * @param g graph representation
+ * @param v vertex to process
+ */
+void process_vertex_late (Graph *g, int v)
 {
 	printf("Processing vertex %d late\n", v);
 	return;
 }
 
-void process_edge (Graph g, Edge *e)
+/**
+ * Template function to process an edge upon traversal
+ * @param g graph representation
+ * @param e edge representation
+ */
+void process_edge (Graph *g, Edge *e)
 {
 	printf("Edge %d - %d\n", e->x, e->y);
 	return;
 }
 
+void init_search (Graph *g)
+{
+	for (int i = 1; i < MAXV + 1; ++i)
+	{
+		g->states[i] = UNDISCOVERED;
+	}
+	printf("Search initialized\n");
+}
+
+/**
+ * Performs a breadth first search on graph g starting at vertex v
+ * @param g the graph representation
+ * @param v the vertex to start from
+ */
 void bfs (Graph g, int v)
+{
+	static int finished = 0;
+
+	if (finished == 1)
+	{
+		printf("Finished BFS\n");
+		return;
+	}
+}
+
+/**
+ * Performs a depth first search on graph g starting at vertex v
+ * @param g the graph representation
+ * @param v the vertex to start from
+ */
+void dfs (Graph *g, int v)
 {
 	static int finished = 0;
 	Edge *edge;
 
 	if (finished == 1)
 	{
+		printf("Finished DFS\n");
 		return;
 	}
 
-	if (g.states[v] == UNDISCOVERED)
+	printf("State[%d]: %d\n", v, g->states[v]);
+	if (g->states[v] == UNDISCOVERED)
 	{
-		g.states[v] = DISCOVERED;
+		g->states[v] = DISCOVERED;
 		process_vertex_early(g, v);
-		edge = g.vertices[v].edges;
-		for (int i = 0; i < g.vertices[v].num_edges; ++i)
+		edge = g->vertices[v].edges;
+		for (int i = 0; i < g->vertices[v].num_edges; ++i)
 		{
 			process_edge(g, edge);
-			if (g.states[edge->y] != PROCESSED)
+			if (g->states[edge->y] != PROCESSED)
 			{
-				bfs(g, edge->y);
+				dfs(g, edge->y);
 			}	
 			edge = edge->next;
 		}
+		process_vertex_late(g, v);
+		g->states[v] = PROCESSED;
 	}
 }
 
@@ -159,12 +208,14 @@ Graph create_random_graph (int max_edges)
 		printf("num edges: %d\t", g.vertices[i].num_edges);
 		print_edges(g, g.vertices[i].edges);	
 	}
-
+	printf("\nCreated random graph\n\n");
 	return g;
 }
 
 int main(int argc, char const *argv[])
 {
 	Graph g = create_random_graph(5);
+	init_search(&g);
+	dfs(&g, 1);
 	return 0;
 }
